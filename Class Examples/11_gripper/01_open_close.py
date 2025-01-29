470011c374425bec7221322d18b86d222494fc63
brokenAvocado <ericyc35@gmail.com>
2025-01-29 15:33:35 -0500

    making P loop script

--- Class Examples/11_gripper/01_open_close.py
+++ Class Examples/11_gripper/01_open_close.py
@@ -20,8 +20,9 @@ from robomaster import robot
 
 
 if __name__ == '__main__':
+    robomaster.config.ROBOT_IP_STR = "192.168.50.121"
     ep_robot = robot.Robot()
-    ep_robot.initialize(conn_type="sta")
+    ep_robot.initialize(conn_type="sta", sn="3JKCH8800100UB")
 
     ep_gripper = ep_robot.gripper
 
