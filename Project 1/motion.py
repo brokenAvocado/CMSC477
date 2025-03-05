53056504a2e512e5b2a720053fe83079f958211a
brokenAvocado <ericyc35@gmail.com>
2025-03-05 16:32:11 -0500

    real shit we about to change

--- Project 1/motion.py
+++ Project 1/motion.py
@@ -21,6 +21,27 @@ class AprilTagDetector: # Given
             camera_params=self.camera_params, tag_size=self.marker_size_m)
         return detections
 
+# X, Y coordinates, X and Y orientation
+tagDict = {
+    30: [[0.532, 1.995, 0], [-1, 0, 0]],
+    31: [[0.798, 1.995, 0], [1, 0, 0]],
+    32: [[0.532, 1.463, 0], [-1, 0, 0]],
+    33: [[0.798, 1.463, 0], [1, 0, 0]],
+    34: [[0.665, 1.064, 0], [0, -1, 0]],
+    35: [[1.197, 2.128, 0], [0, -1, 0]],
+    36: [[1.729, 2.128, 0], [0, -1, 0]],
+    37: [[1.463, 1.33, 0], [0, 1, 0]],
+    38: [[1.33, 0.931, 0], [-1, 0, 0]],
+    39: [[1.596, 0.931, 0], [1, 0, 0]],
+    40: [[1.33, 0.399, 0], [-1, 0, 0]],
+    41: [[1.596, 0.399, 0], [1, 0, 0]],
+    42: [[2.128, 1.995, 0], [-1, 0, 0]],
+    43: [[2.394, 1.995, 0], [1, 0, 0]],
+    44: [[2.128, 1.463, 0], [-1, 0, 0]],
+    45: [[2.394, 1.463, 0], [1, 0, 0]],
+    46: [[2.261, 1.064, 0], [0, -1, 0]]
+}
+
 '''
 Desc: gets the april tag orientation in Euler Angles (Roll, Pitch, Yaw)
 and position of the tag with corrected pitch (accounting for camera)
@@ -35,9 +56,9 @@ def get_pose_apriltag_in_camera_frame(detection):
     roll = np.arctan2(R_ca[1][0],R_ca[0][0])
     yaw = np.arctan2(-R_ca[2][0],np.sqrt(R_ca[2][1]**2+R_ca[2][2]**2))
     pitch = np.arctan2(R_ca[2][1],R_ca[2][2])
-    const = 1 #180/np.pi
+    const = 180/np.pi
     
-    rotation = [roll, yaw, pitch]
+    rotation = [const*roll, const*yaw, const*pitch]
 
     t_ca = t_ca.flatten()
     t_ca[2] = t_ca[2]*np.cos(pitch)
@@ -135,7 +156,7 @@ def closest(detections):
 Desc: converts the position in the maze to the real world
 
 Input: 
-- posTag: the position of the desired tag
+- posTag: the position of the robot relative to the tag
 - tag_id: the id of the desired tag
 - matrixMap: the matrix representation of the maze
 Output:
@@ -148,21 +169,22 @@ def relative2world(posTag, tag_id, matrixMap):
     for row in matrixMap:
         for col in row:
             if col == str(tag_id):
-                index[0] = row
-                index[1] = col
+                index.append(matrixMap.index(row))
+                index.append(row.index(col))
+    # print(index)
+    print(tag_id)
 
-    robotX = col*scalingFactor-scalingFactor-posTag[0]
-    robotY = row*scalingFactor-scalingFactor-posTag[1]
+    robotX = index[0]*scalingFactor-scalingFactor-posTag[0]
+    robotY = index[1]*scalingFactor-scalingFactor-posTag[2]
+    gridX = robotX*scalingFactor
+    gridY = robotY*scalingFactor
 
-    return robotX, robotY
+    return robotX, robotY, gridX, gridY
 
 def detect_tag_loop(ep_camera, apriltag):
-    scaling_factor = 0.266/3
-
-    pathfinding = DJI("lab1.csv")
-
-    pathfinding.search()
-    ax = pathfinding.plot()
+    pathfinding = DJI("Project 1\\Lab1.csv")
+    # pathfinding.search()
+    # ax = pathfinding.plot()
         
     while True:
         try:
@@ -177,8 +199,14 @@ def detect_tag_loop(ep_camera, apriltag):
         detections = apriltag.find_tags(gray)
 
         if len(detections) > 0:
-            tags = closest(detections)
-            print(tags.tag_id)
+            tag = closest(detections)
+            pos, rot = get_pose_apriltag_in_camera_frame(tag)
+            print(pos)
+            print(rot)
+            # x, y, _, _ = relative2world(pos, tag.tag_id, pathfinding.matrix)
+            # print(x)
+            # print(y)
+            # print(tags.tag_id)
 
         draw_detections(img, detections)
         cv2.imshow("img", img)
@@ -238,35 +266,48 @@ def motionControl(ep_robot, ep_chassis, ep_camera, apriltag):
             plt.show()
             break
     
-if __name__ == '__main__':
-    path = DJI("Project 1\\lab1.csv")
-    path.search()
-    path.plot(True)
-
 # if __name__ == '__main__':
-#     # More legible printing from numpy.
-#     np.set_printoptions(precision=3, suppress=True, linewidth=120)
+#     path = DJI("Project 1\\lab1.csv")
+#     path.search()
+#     path.plot(True)
 
-#     robomaster.config.ROBOT_IP_STR = "192.168.50.121"
-#     ep_robot = robot.Robot()
-#     ep_robot.initialize(conn_type="sta", sn="3JKCH8800100UB")
+if __name__ == '__main__':
+    # path = DJI("Project 1\\lab1.csv")
+    # begin = 30
+    # end = 46
+    # scalingFactor = 0.266/3
+    
+    # for i in range(30, 46):
+    #     print("For ID: " + str(i))
+    #     for row in path.matrix:
+    #         for col in row:
+    #             if col == str(i):
+    #                 print(path.matrix.index(row)*scalingFactor)
+    #                 print(row.index(col)*scalingFactor)
+    
+    # More legible printing from numpy.
+    # np.set_printoptions(precision=3, suppress=True, linewidth=120)
 
-#     ep_chassis = ep_robot.chassis
-#     ep_camera = ep_robot.camera
-#     ep_camera.start_video_stream(display=False, resolution=camera.STREAM_360P)
+    robomaster.config.ROBOT_IP_STR = "192.168.50.121"
+    ep_robot = robot.Robot()
+    ep_robot.initialize(conn_type="sta", sn="3JKCH8800100UB")
 
-#     K = np.array([[314, 0, 320], [0, 314, 180], [0, 0, 1]]) # Camera focal length and center pixel
-#     marker_size_m = 0.153 # Size of the AprilTag in meters
-#     apriltag = AprilTagDetector(K, threads=2, marker_size_m=marker_size_m)
+    ep_chassis = ep_robot.chassis
+    ep_camera = ep_robot.camera
+    ep_camera.start_video_stream(display=False, resolution=camera.STREAM_360P)
 
-#     try:
-#         detect_tag_loop(ep_camera, apriltag)
-#     except KeyboardInterrupt:
-#         pass
-#     except Exception as e:
-#         print(traceback.format_exc())
-#     finally:
-#         print('Waiting for robomaster shutdown')
-#         ep_camera.stop_video_stream()
-#         ep_robot.close()
+    K = np.array([[314, 0, 320], [0, 314, 180], [0, 0, 1]]) # Camera focal length and center pixel
+    marker_size_m = 0.153 # Size of the AprilTag in meters
+    apriltag = AprilTagDetector(K, threads=2, marker_size_m=marker_size_m)
+
+    try:
+        detect_tag_loop(ep_camera, apriltag)
+    except KeyboardInterrupt:
+        pass
+    except Exception as e:
+        print(traceback.format_exc())
+    finally:
+        print('Waiting for robomaster shutdown')
+        ep_camera.stop_video_stream()
+        ep_robot.close()
 
